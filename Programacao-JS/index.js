aaa9773569538341c07a26a33fb3ec121a81d317
Priscila-Alvesc <alvescostapriscila@gmail.com>
2025-04-05 13:39:36 -0300

    commit

---
+++ Programacao-JS/index.js
@@ -0,0 +1,44 @@
+/*
+*Console API
+*/
+
+console.log("Aula inicial")
+console.error("Falha na execução")
+console.warn("Atenção")
+console.table([{
+    nome: 'Priscila',
+    turma: '02',
+    disciplina: 'Prog_JS'
+    },
+    {
+        nome: 'Jose',
+        turma: '02',
+        disciplina: 'Prog_JS'
+    }])
+
+/*
+*Constantes e Variáveis
+*/
+
+//const - para informações que nunca mudam
+//var ou let - para informações que podem mudar
+
+//informações que não mudam
+const raca  = 'maltês'
+const nome = 'Lua'
+const sexo = 'Femea'
+const cor = 'Branca'
+const dataNascimento = '03/05/2021'
+const porte = 'P'
+
+//informações que podem mudar
+let peso = 5.0
+let idade = 4
+let vacinado =  true  
+let castrado = true
+let tamanho = 'P'
+
+console.log('O peso é: ' + peso);
+
+
+
