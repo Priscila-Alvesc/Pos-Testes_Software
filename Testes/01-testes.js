ec2708546b1851fd96176f370e0f87c83cae57cc
alvescostapriscila@gmail.com <alvescostapriscila@gmail.com>
2025-05-10 18:59:18 -0300

    ok

--- Testes/01-testes.js
+++ Testes/01-testes.js
@@ -1,25 +1,27 @@
 /* 
-describe -> agrupador de testes
-it -> implementação do teste
+describe -> agrupador de testes (descrever)
+it -> implementação do teste (isto)
 TDD
-    crio o teste
-    vejo o teste falhar
-    crio a implementação para o teste passar
-    rodo o teste de novo
-    refatoro o código
-    ASSERTION/ASSERCAO
-     verificar se um comportamento está de acordo com o esperado
+    crio o teste (criar o teste)
+    vejo o teste falhar (ver o teste falhar)
+    crio a implementação para o teste passar (criar a implementação para o teste passar)
+    rodo o teste de novo (rodar o teste novamente)
+    refatoro o código (refatorar o código)
+    ASSERTION/ASSERCAO (AFIRMAÇÃO/ASSERÇÃO)
+     verificar se um comportamento está de acordo com o esperado (verificar se um comportamento está de acordo com o esperado)
 */
-import { exibirNomeDogFormatado } from '../conceitos/testes-de-unidade.js'
+
+import { exibirNomeDogFormatado } from '../Programacao-JS/conceitos/testes-de-unidade.js'
 import assert from 'node:assert'
-describe('Testes do Projeto', () => {
-    it('deve exibir o nome do dog com letras maiúsculas', () => {
+
+describe('Testes do Projeto', () => { // Ou, mais descritivo: 'Testes da Unidade de Formatação de Nomes de Cachorros'
+    it('deve exibir o nome do cachorro com letras maiúsculas', () => {
         assert.strictEqual(exibirNomeDogFormatado('Mimosa'), 'MIMOSA')
         // verifique se: valor atual é igual ao valor esperado
     })
-    it('primeiro teste', () => {
+    it('primeiro teste', () => { // Exemplo: 'primeiro teste de exemplo'
     })
-    it('segundo teste', () => {
+    it('segundo teste', () => { // Exemplo: 'segundo teste de exemplo que deve falhar'
         throw new Error()
     })
 })
---
+++ trabalho.js
@@ -0,0 +1,14 @@
+/*
+geradorDeTagsDeIdentificacao,
+  verificarSePodeSerAdotado,
+  calcularConsumoDeRacao,
+  decidirTipoDeAtividadePorPorte,
+  buscarDadoAsync
+*/
+
+function geradorDeTagsDeIdentificacao (nome) {
+    return nome.toUpperCase();
+}
+export { 
+    geradorDeTagsDeIdentificacao 
+}
