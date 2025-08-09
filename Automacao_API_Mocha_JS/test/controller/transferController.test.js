3c367cc45876655190b7874f7b8f232f2b04403d
alvescostapriscila@gmail.com <alvescostapriscila@gmail.com>
2025-08-09 18:10:54 -0300

    Mocks para testes controller

--- Automacao_API_Mocha_JS/test/controller/transferController.test.js
+++ Automacao_API_Mocha_JS/test/controller/transferController.test.js
@@ -41,5 +41,28 @@ describe('Transfer Controller', () => {
             expect(resposta.status).to.equal(400);
             expect(resposta.body).to.have.property('error', 'Remetente ou destinatário inválido');
         });
+
+        it('Usando Mocks: Quando informo valores válidos eu tenho sucesso com 201 CREATED', async () => {
+            const fakeTransfer = {
+                remetente: "julio",
+                destinatario: "priscila",
+                valor: 999,
+                data: new Date().toISOString()
+            };
+            sinon.stub(transferService, 'transfer').returns({ transfer: fakeTransfer });
+
+            const resposta = await request(app)
+                .post('/transfer')
+                .send({
+                    "remetente": "julio",
+                    "destinatario": "priscila",
+                    "valor": 1000
+                });
+            expect(resposta.status).to.equal(201);
+            expect(resposta.body).to.have.property('transfer');
+            expect(resposta.body.transfer).to.have.property('remetente', 'julio');
+            expect(resposta.body.transfer).to.have.property('destinatario', 'priscila');
+            expect(resposta.body.transfer).to.have.property('valor', 999);
+        });
     });
 });
