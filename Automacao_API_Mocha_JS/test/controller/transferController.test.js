01d979e1dfcfcec5f41f77bbe2a95887b8f002bd
alvescostapriscila@gmail.com <alvescostapriscila@gmail.com>
2025-08-09 17:51:48 -0300

    Mocks para testes controller

--- Automacao_API_Mocha_JS/test/controller/transferController.test.js
+++ Automacao_API_Mocha_JS/test/controller/transferController.test.js
@@ -6,10 +6,31 @@ const { expect } = require('chai');
 // Aplicação
 const app = require('../../app');
 
+//Mock
+const transferService = require ('../../service/transferService');
+
 // Testes
 describe('Transfer Controller', () => {
+    afterEach(() => {
+        sinon.restore();
+    });
+
     describe('POST /transfer', () => {
         it('Quando informo remetente e destinatário inexistentes recebo 400', async () => {
+            const resposta = await request(app)
+                .post('/transfer')
+                .send({
+                    "remetente": "julio",
+                    "destinatario": "priscila",
+                    "valor": 1000
+                });
+            expect(resposta.status).to.equal(400);
+            expect(resposta.body).to.have.property('error', 'Remetente ou destinatário inválido');
+        });
+
+        it('Usando Mocks: Quando informo remetente e destinatário inexistentes recebo 400', async () => {
+            sinon.stub(transferService, 'transfer').returns({ error: 'Remetente ou destinatário inválido' });
+
             const resposta = await request(app)
                 .post('/transfer')
                 .send({
